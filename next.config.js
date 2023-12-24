bf0a78248ddcd3009a429b6481e74ab03c3cdc12
pmbjojo <jourdamj@gmail.com>
2023-12-24 19:03:45 +0100

    chore: add images path to config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default config;
