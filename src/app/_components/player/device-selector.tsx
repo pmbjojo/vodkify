16d5d5a8b5524f049fba03a80aeb1d19a34f260d
pmbjojo <jourdamj@gmail.com>
2023-12-25 04:29:41 +0100

    style: add classname support

--- src/app/_components/player/device-selector.tsx
+++ src/app/_components/player/device-selector.tsx
@@ -7,17 +7,24 @@ import {
   SelectLabel,
   SelectItem,
 } from "@/components/ui/select";
+import { cn } from "@/lib/utils";
 import { api } from "@/trpc/react";
 import React from "react";
 
-export default function DeviceSelector({ device }: { device: string | null }) {
+export default function DeviceSelector({
+  currentDevice,
+  className,
+}: Readonly<{
+  currentDevice: string | null;
+  className?: string;
+}>) {
   const { data: devices } = api.spotify.getAvailableDevices.useQuery();
   const { mutate: transferPlayback } =
     api.spotify.transferPlayback.useMutation();
   return (
     <Select onValueChange={(device) => transferPlayback({ device })}>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder={device} />
+      <SelectTrigger className={cn(className)}>
+        <SelectValue placeholder={currentDevice} />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
